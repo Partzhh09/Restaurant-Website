1b1f1af361b4d3ff2c6dd90a2cf4b93879d13b3c
Partzhh09 <parthchotaliya788@gmail.com>
2025-04-06 11:00:42 +0530

    added some files

--- Script/script.js
+++ Script/script.js
@@ -1,9 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
     let carousel = document.getElementById("restaurantCarousel");
     let title = document.getElementById("dish-title");
     let description = document.getElementById("dish-description");
 
-    carousel.addEventListener("slid.bs.carousel", function () {
+    carousel.addEventListener("slid.bs.carousel", () => {
         let activeItem = document.querySelector(".carousel-item.active");
         title.textContent = activeItem.dataset.title;
         description.textContent = activeItem.dataset.description;
